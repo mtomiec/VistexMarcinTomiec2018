62495badb1936abb69b756bedbe87729677c8b80
mtomiec <mtomiec@gmail.com>
2018-05-22 23:38:37 +0200

    Counting surface working

---
+++ VistexExercise/Polygon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VistexExercise
+{
+    class Polygon
+    {
+        public int numberOfVertices { get; }
+        public double lenghtOfSide { get; }
+        public string name { get; }
+
+        public Polygon(int nov, double los, string n)
+        {
+            numberOfVertices = nov;
+            lenghtOfSide = los;
+            name = n;
+        }
+
+        public double CalculateSurface()
+        {
+            double circumRadius = lenghtOfSide / (2 * Math.Sin(Math.PI / numberOfVertices));
+            double inscribedCircleRadius = circumRadius * Math.Cos(Math.PI/numberOfVertices);
+            return numberOfVertices * lenghtOfSide * inscribedCircleRadius / 2;
+        }
+    }
+}
---
+++ VistexExercise/PolygonFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace VistexExercise
+{
+    class PolygonFactory
+    {
+        public Polygon CreatePolygon(int nov, double los)
+        {
+            if (nov < 3)
+            {
+                Console.WriteLine("It's impossible to make polygon with {0} vertices", nov);
+                return null;
+            }
+            else
+            {
+                switch (nov)
+                {
+                    case 3:
+                        return new Polygon(nov, los, "Triangle");
+                    case 4:
+                        return new Polygon(nov, los, "Square");
+                    case 5:
+                        return new Polygon(nov, los, "Pentagon");
+                    case 6:
+                        return new Polygon(nov, los, "Hexagon");
+                    case 7:
+                        return new Polygon(nov, los, "Heptagon");
+                    case 8:
+                        return new Polygon(nov, los, "Octagon");
+                    case 9:
+                        return new Polygon(nov, los, "Nonagon");
+                    case 10:
+                        return new Polygon(nov, los, "Decagon");
+                    default:
+                        return new Polygon(nov, los, nov.ToString()+"-gon");
+
+                }
+            }
+
+        }
+    }
+}
